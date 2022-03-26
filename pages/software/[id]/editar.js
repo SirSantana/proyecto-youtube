404fc8baf86ee009d07e0817bbb4d294f73d0dea
sirSantana23 <salazarmiguelito23gmail.com>
2022-03-26 10:13:00 -0500

    getServerSideProps en vez de SWR

--- pages/software/[id]/editar.js
+++ pages/software/[id]/editar.js
@@ -2,7 +2,6 @@ import Form from "../../../components/Form";
 import Layout from "../../../components/Layout";
 import useSWR from "swr";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 
 const fetcher = async (url) => {
   const res = await fetch(url);
@@ -17,34 +16,49 @@ const fetcher = async (url) => {
 
   return data;
 };
-export default function Editar() {
+export default function Editar({data}) {
   const router = useRouter();
   const { id } = router.query;
+  console.log(id);
+  const {pregunta, description, link} = data.data;
 
-  console.log(id)
-  const { data: software, error } = useSWR(
-    id ? `http://localhost:3000/api/software/${id}` : null,
-    fetcher
-  );
-  useEffect(()=>{
-  },[software])
+  // console.log(id)
+  // const { data: software, error } = useSWR(
+  //   id ? `http://localhost:3000/api/software/${id}` : null,
+  //   fetcher
+  // );
 
-  
-  const formm = {
-    pregunta: software.pregunta,
-    description: software.description,
-    link: software.link,
-  };
-  if (error)
+  if (!data)
     return (
       <Layout>
-        <h2>Error</h2>
+        <h2>Loading...</h2>
       </Layout>
     );
+  const formm = {
+    pregunta: pregunta,
+    description: description,
+    link: link,
+  };
+  // if (error)
+  //   return (
+  //     <Layout>
+  //       <h2>Error</h2>
+  //     </Layout>
+  //   );
   return (
     <Layout>
-
       <Form newQuest={false} formm={formm} route={"/software"} url={`http://localhost:3000/api/software/${id}`}/>
     </Layout>
   );
+}
+export async function getServerSideProps({params}){
+  try {
+    const res = await fetch(params.id ? `http://localhost:3000/api/software/${params.id}`: null)
+    const data = await res.json()
+    return {
+      props: {data}
+    }
+  } catch (error) {
+   console.log(error); 
+  }
 }
